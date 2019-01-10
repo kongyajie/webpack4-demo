e6ff04300f41ff70334f425953438d1ed725a165
Aaron <kongyajie0704@gmail.com>
2019-01-10 10:12:28 +0800

    finish demo v1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import _ from 'lodash';
+import './css/style.css';
+import './css/style2.css';
+import Icon from './images/logo.jpg';
+import data from './js/data.json';
 
 function component() {
     let element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack4'], ' ');
+    element.classList.add('hello');
+
+    // 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    
+    element.appendChild(myIcon)
+
+    // JSON 数据
+    var dataDiv = document.createElement('div');
+    dataDiv.innerHTML = JSON.stringify(data);
+    element.appendChild(dataDiv);
   
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,61 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    another: './src/another-module.js'
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              
+            }
+          },
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Managemet'
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 };
